3d058beaaae6e0bfde2430b9b59cd6bb568bce5d
twotop6357 <twotop6357@gmail.com>
2024-05-20 17:07:24 +0900

    Add RankingSystem
    랭킹 시스템 추가

---
+++ Assets/Scripts/IntroButtonController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroButtonController : MonoBehaviour
+{
+    public TMP_InputField inputField;
+    [SerializeField] private GameObject rankingPanel;
+
+    public void StartBtn()
+    {
+        if (inputField.text.Length < 1 || inputField.text.Length > 10)
+        {
+            return;
+        }
+
+        DataManager.instance.userName = inputField.text;
+
+        SceneManager.LoadScene("Main");
+    }
+    public void RankingBtn()
+    {
+        rankingPanel.SetActive(true);
+    }
+    public void CloseRankingPanelBtn()
+    {
+        rankingPanel.SetActive(false);
+    }
+}
